efd5a097521634fbd465b0f9c4d3480fdd467b00
GTHANEKA <GTHANEKA@LIN51013439.corp.capgemini.com>
2023-06-23 12:53:54 +0530

    First commit

---
+++ src/test/java/com/cg/gas/booking/GasBooking/GasBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.gas.booking.GasBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
